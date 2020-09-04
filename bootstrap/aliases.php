81a467a29d4e7855a0132c6edea019131e398b09
Arturo Ramos <ircsasw@gmail.com>
2020-09-03 20:12:57 -0500

    path to projects builds env

--- bootstrap/aliases.php
+++ bootstrap/aliases.php
@@ -15,4 +15,13 @@ Yii::setAlias('@admin', ConfigKit::config()->getBasePath() . DIRECTORY_SEPARATOR
 
 Yii::setAlias('@api', ConfigKit::config()->getBasePath() . DIRECTORY_SEPARATOR . '../api');
 
-Yii::setAlias('@resources', ConfigKit::config()->getBasePath() . DIRECTORY_SEPARATOR . '../projects');
+Yii::setAlias('@resources', call_user_func(
+    function (): string {
+        $envResources = strval(ConfigKit::env()->get('DIR_PROJECTS')) ?: 'projects';
+        if (DIRECTORY_SEPARATOR === substr($envResources, 0, 1)) { // absolute path
+            return $envResources;
+        }
+        // relative path
+        return dirname(ConfigKit::config()->getBasePath()) . DIRECTORY_SEPARATOR . $envResources;
+    }
+));
